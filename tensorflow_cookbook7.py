540a418bd96c2aff465b910d067687b0eb765505
Olga Belitskaya <safuolga@gmail.com>
2021-02-04 14:26:05 +0300

    update tensorflow_cookbook7.ipynb

--- tensorflow_cookbook7.py
+++ tensorflow_cookbook7.py
@@ -39,8 +39,24 @@ def cmap_header(params):
 # Commented out IPython magic to ensure Python compatibility.
 # %cmap_header Code Modules & Functions
 
-import tensorflow as tf,tensorflow_hub as hub,numpy as np
-def esrgantf2_superresolution(img_path,img_size=50):
+import tensorflow as tf,tensorflow_hub as hub
+import pylab as pl,numpy as np
+
+def get_resize_img(img_path,img_size=50):
+    img_path=tf.keras.utils.get_file(
+        'img'+str(np.random.randint(1,99999))+'.png',img_path)
+    lr=tf.io.read_file(img_path)
+    lr=tf.image.decode_jpeg(lr)
+    print('mean: %f'%lr.numpy().mean())
+    lr=tf.image.resize(lr,[img_size,img_size])
+    lr=tf.expand_dims(lr.numpy()[:,:,:3],axis=0)
+    return tf.cast(lr,tf.float32)
+
+def esrgantf2_superresolution(lr):
+    if len(lr.shape)<4:
+        lr=tf.expand_dims(lr.numpy()[:,:,:3],axis=0)
+    lr=tf.cast(lr,tf.float32)
+    img_size=lr.shape[1]
     model=hub.load('https://tfhub.dev/captain-pool/esrgan-tf2/1')
     func=model.signatures[tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY]
     func.inputs[0].set_shape([1,img_size,img_size,3])
@@ -50,14 +66,6 @@ def esrgantf2_superresolution(img_path,img_size=50):
     with tf.io.gfile.GFile('ESRGAN.tflite','wb') as f:
         f.write(tflite_model)
     esrgan_model_path='./ESRGAN.tflite'
-    img_path=tf.keras.utils.get_file(
-        'img'+str(np.random.randint(1,99999))+'.png',img_path)
-    lr=tf.io.read_file(img_path)
-    lr=tf.image.decode_jpeg(lr)
-    print('mean: %f'%lr.numpy().mean())
-    lr=tf.image.resize(lr,[img_size,img_size])
-    lr=tf.expand_dims(lr.numpy()[:,:,:3],axis=0)
-    lr=tf.cast(lr,tf.float32)
     interpreter=tf.lite.Interpreter(model_path=esrgan_model_path)
     interpreter.allocate_tensors()
     input_details=interpreter.get_input_details()
@@ -66,32 +74,63 @@ def esrgantf2_superresolution(img_path,img_size=50):
     interpreter.invoke()
     output_data=interpreter.get_tensor(output_details[0]['index'])
     sr=tf.squeeze(output_data,axis=0)
-    sr=tf.clip_by_value(sr,0,255)
-    sr=tf.round(sr); sr=tf.cast(sr,tf.uint8)
+    sr=tf.round(tf.clip_by_value(sr,0,255)) 
+    sr=tf.cast(sr,tf.uint8)
     lr=tf.cast(tf.squeeze(lr,axis=0),tf.uint8)
     return lr,sr
 
-import pylab as pl
-def low2superbicubic_imgs(lr,sr):
-    pl.figure(figsize=(8,4)); pl.title('LR')
-    pl.imshow(lr.numpy()); pl.show(); print()
-    pl.figure(figsize=(8,4))
-    pl.subplot(1,2,1); pl.title(f'ESRGAN (x4)')
-    pl.imshow(sr.numpy());
-    img_size=lr.shape[1]
-    bicubic=tf.image.resize(
-        lr,[img_size*4,img_size*4],tf.image.ResizeMethod.BICUBIC)
-    bicubic=tf.cast(bicubic, tf.uint8)
-    pl.subplot(1,2,2); pl.title('Bicubic')
-    pl.imshow(bicubic.numpy())
+def low2bicubic_low2super(lr0,sr1,sr2):
+    img_size=lr0.shape[1]
+    lr0=tf.cast(tf.squeeze(lr0,axis=0),tf.uint8)
+    pl.figure(figsize=(15,5))
+    pl.subplot(1,3,1); pl.title('LR0')
+    pl.imshow(lr0.numpy())
+    bicubic4=tf.image.resize(
+        lr0,[img_size*4,img_size*4],
+        tf.image.ResizeMethod.BICUBIC)
+    bicubic4=tf.cast(bicubic4,tf.uint8)
+    pl.subplot(1,3,2); pl.title(f'Bicubic x4')
+    pl.imshow(bicubic4.numpy())
+    bicubic16=tf.image.resize(
+        bicubic4,[img_size*16,img_size*16],
+        tf.image.ResizeMethod.BICUBIC)
+    bicubic16=tf.cast(bicubic16,tf.uint8)
+    pl.subplot(1,3,3); pl.title(f'Bicubic x16')
+    pl.imshow(bicubic16.numpy())
+    pl.tight_layout(); pl.show()
+    pl.figure(figsize=(15,5))
+    pl.subplot(1,3,1); pl.title('LR0')
+    pl.imshow(lr0.numpy())   
+    pl.subplot(1,3,2); pl.title(f'ESRGAN x4')
+    pl.imshow(sr1.numpy())
+    pl.subplot(1,3,3); pl.title(f'ESRGAN x16')
+    pl.imshow(sr2.numpy())
     pl.tight_layout(); pl.show()
 
 # Commented out IPython magic to ensure Python compatibility.
 # %cmap_header ESRGAN Super Resolution
 
 file_path='https://olgabelitskaya.gitlab.io/images/'
-lr,sr=esrgantf2_superresolution(file_path+'04_001.png',50)
-low2superbicubic_imgs(lr,sr)
+file_name='04_001.png'
+lr0=get_resize_img(file_path+file_name,50)
+lr0.shape
 
-lr,sr=esrgantf2_superresolution(file_path+'01_019.png',50)
-low2superbicubic_imgs(lr,sr)
+lr1,sr1=esrgantf2_superresolution(lr0)
+lr1.shape,sr1.shape
+
+lr2,sr2=esrgantf2_superresolution(sr1)
+lr2.shape,sr2.shape
+
+low2bicubic_low2super(lr0,sr1,sr2)
+
+file_name='01_019.png'
+lr0=get_resize_img(file_path+file_name,50)
+lr0.shape
+
+lr1,sr1=esrgantf2_superresolution(lr0)
+lr1.shape,sr1.shape
+
+lr2,sr2=esrgantf2_superresolution(sr1)
+lr2.shape,sr2.shape
+
+low2bicubic_low2super(lr0,sr1,sr2)
