8037153973c9e24e68e74417dcfc366e22ef4fb9
Olga Belitskaya <safuolga@gmail.com>
2021-03-09 10:36:44 +0300

    update advanced_outputs.ipynb

--- advanced_outputs.py
+++ advanced_outputs.py
@@ -9,7 +9,8 @@ Original file is located at
 # ✒️ Executing JavaScript & HTML from Python
 """
 
-from IPython.display import display,HTML,Javascript,JSON
+from IPython.display import display,HTML,Javascript,JSON,Image
+from IPython.core.magic import register_line_magic
 from google.colab import output
 
 display(Javascript("""
@@ -59,6 +60,33 @@ var tc=setInterval(function() {
 """
 display(HTML(html_str))
 
+# Commented out IPython magic to ensure Python compatibility.
+@register_line_magic
+def cmap_header(params):
+    params=params.split('|'); string=params[0]
+    if len(params)==1: 
+        font_size='20'; font_family='Akronim'; cmap='Sinebow'
+    elif  len(params)==2: 
+        font_size=params[1]; font_family='Akronim'; cmap='Sinebow'
+    elif  len(params)==3: 
+        font_size=params[1]; font_family=params[2]; cmap='Sinebow'
+    else: 
+        font_size=params[1]; font_family=params[2]; cmap=params[3]
+    html_str="""
+    <head><script src='https://d3js.org/d3.v6.min.js'></script></head>
+    <style>@import 'https://fonts.googleapis.com/css?family="""+\
+    font_family+"""&effect=3d'; #colorized {font-family:"""+font_family+"""; 
+    color:white; padding-left:10px; font-size:"""+font_size+"""px;}</style>
+    <h1 id='colorized' class='font-effect-3d'>"""+string+"""</h1>
+    <script>
+    var tc=setInterval(function(){
+        var now=new Date().getTime();
+        var iddoc=document.getElementById('colorized');
+        iddoc.style.color=d3.interpolate"""+cmap+"""(now%(60000)/60000);},1)
+    </script>"""
+    display(HTML(html_str))
+# %cmap_header CODE MODULES & HELPFUL TOOLS|24|Ewert|Turbo
+
 """#✒️ Tagged Outputs"""
 
 import sys,time
@@ -81,7 +109,7 @@ starts=[17,67,52,35,88]; n=len(starts)
 sliders=[iw.IntSlider(starts[i],min=0,max=100) 
          for i in range(n)]
 colors=[(i/5.,i/10.,.75) for i in range(n)]
-@widgets.interact(
+@iw.interact(
     v0=sliders[0],v1=sliders[1],v2=sliders[2],
     v3=sliders[3],v4=sliders[4])
 def render(v0,v1,v2,v3,v4):
@@ -105,11 +133,11 @@ def pd_style():
                         ('text-shadow','3px 3px 3px #aaa')])]
 
 source=data.stocks()
-stock_picker=widgets.SelectMultiple(
+stock_picker=iw.SelectMultiple(
     options=source.symbol.unique(),
     value=list(source.symbol.unique()),
     description='SYMBOLS')
-@widgets.interact(symbols=stock_picker)
+@iw.interact(symbols=stock_picker)
 def render(symbols):
   selected=source.symbol.isin(list(symbols))
   display(source[selected].head(3).T\
@@ -117,3 +145,80 @@ def render(symbols):
   chart=alt.Chart(source[selected]).mark_line().encode(
       x='date',y='price',color='symbol',strokeDash='symbol')
   return chart
+
+"""# ✒️ Images & Animations"""
+
+import cv2,numpy as np,imageio,PIL.Image
+from skimage import io,transform
+from google.colab.patches import cv2_imshow
+
+!curl -o 001.png https://olgabelitskaya.gitlab.io/images/01_001.png
+!curl -o 03_012.png https://olgabelitskaya.gitlab.io/images/03_012.png
+!curl -o 03_015.png https://olgabelitskaya.gitlab.io/images/03_015.png
+
+img=cv2.imread('001.png',cv2.IMREAD_UNCHANGED)
+cv2_imshow(img)
+
+file1,file2='03_012.png','03_015.png'
+def create_coords(file1,file2):
+    img1,img2=io.imread(file1),io.imread(file2)
+    imgbw1,imgbw2=np.ones(img1.shape[:2]),np.ones(img2.shape[:2])
+    imgbw1[img1[:,:,1]<200],imgbw2[img2[:,:,1]<200]=0,0
+    coord1=np.array(np.where(imgbw1<1)).T; coord=coord1
+    coord2=np.array(np.where(imgbw2<1)).T
+    if coord1.shape[0]>coord2.shape[0]: coord1,coord2=coord2,coord
+    while not coord2.shape==coord1.shape:
+        randi=np.random.randint(
+            0,coord2.shape[0]-1,coord2.shape[0]-coord1.shape[0])
+        coord2=np.delete(coord2,list(randi),axis=0)
+    coords=[]
+    for el in [coord1,coord2]:
+        fx=el[:,0]; fy=el[:,1]
+        fx=(fx-fx.min())/(fx.max()-fx.min())
+        fy=(fy-fy.min())/(fy.max()-fy.min())
+        el=np.array([[fx[i],fy[i]] for i in range(len(fx))])
+        coords.append(el)
+    return np.array(coords)
+coords=create_coords(file1,file2)
+steps=30; img_size_out=256
+def interpolate_hypersphere(v1,v2,steps):
+    v1norm=np.linalg.norm(v1); v2norm=np.linalg.norm(v2)
+    vectors=[]; v2normalized=v2*(v1norm/v2norm)
+    for step in range(steps):
+        interpolated=v1+(v2normalized-v1)*step/(steps-int(1))
+        interpolated_norm=np.linalg.norm(interpolated)
+        interpolated_normalized=interpolated*(v1norm/interpolated_norm)
+        vectors.append(interpolated_normalized)
+    return np.array(vectors)
+coords_int=np.vstack([interpolate_hypersphere(coords[1],coords[0],steps),
+                      interpolate_hypersphere(coords[0],coords[1],steps)])
+def create_images(coords_int,img_size_out,img_size=2048):
+    imgs=[]
+    for i in range(coords_int.shape[0]):
+        fx=coords_int[i,:,0]; fy=coords_int[i,:,1]
+        fx=int(.05*img_size)+np.array(np.clip(
+            .9*fx*img_size,0,int(.95*img_size)),dtype='int32')
+        fy=int(.05*img_size)+np.array(np.clip(
+            .9*fy*img_size,0,int(.95*img_size)),dtype='int32')
+        f=np.array([[fx[i],fy[i]] for i in range(len(fx))])
+        img=np.zeros((img_size,img_size))
+        for [x,y] in f: img[x,y]=255
+        imgs.append(transform.resize(img,(img_size_out,img_size_out)))
+    return np.array(imgs)
+imgs=create_images(coords_int,img_size_out)
+
+fig=pl.figure(figsize=(8,8))
+fig.add_subplot(2,2,1)
+pl.scatter(coords_int[0][:,0],coords_int[0][:,1],s=.1,c='g')
+fig.add_subplot(2,2,2)
+pl.scatter(coords_int[30][:,0],coords_int[30][:,1],s=.1,c='g')
+fig.add_subplot(2,2,3); pl.imshow(imgs[0],cmap='Greens')
+fig.add_subplot(2,2,4); pl.imshow(imgs[30],cmap='Greens')
+pl.tight_layout();
+
+ch_imgs=np.zeros([60,256,256,3])
+ch_imgs[:,:,:,1]=imgs
+ch_imgs=np.array(ch_imgs*255,dtype=np.uint8)
+file_name='pic.gif'
+imageio.mimsave(file_name,ch_imgs)
+Image(open(file_name,'rb').read())
