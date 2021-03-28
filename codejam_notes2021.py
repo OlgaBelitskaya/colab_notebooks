0f596fbe18026dc51e45495f4542f14cfbb04baa
Olga Belitskaya <safuolga@gmail.com>
2021-03-28 22:16:14 +0300

    add codejam_notes2021.ipynb

--- codejam_notes2021.py
+++ codejam_notes2021.py
@@ -509,9 +509,7 @@ so there is no way the total cost can be as low as required.
 """
 
 import itertools,numpy,sympy
-from sympy.combinatorics import permutations
 from sympy.utilities.iterables import multiset_permutations
-from sympy.utilities.iterables import multiset_combinations
 sympy.init_printing(use_unicode=True)
 
 C=0; S=['☜','☞','☝︎','☟']
@@ -531,3 +529,231 @@ C=0; S=['☜','☞','☝︎','☟']
 S=list(permutate(S))
 for s in S: print(s); C+=1
 C==sympy.factorial(4)
+
+def cost(N):
+    COST=0
+    for i in range(len(N)-1):
+        j=i+N[i:].index(min(N[i:]))
+        if i==j:
+            COST+=1 
+        else:
+            L=[N[k] for k in range(i)]
+            L+=[N[j-k] for k in range(j-i+1)]
+            L+=[N[k] for k in range(j+1,len(N))]
+            COST+=j-i+1
+            N=L
+    return COST
+def max_cost(L):
+    if L==2: return 2
+    else: return L+max_cost(L-1)
+def max_res(L):    
+    res=[2,1]
+    for i in range(3,L+1):
+        p=len(res)//2
+        res=res[:p]+[i]+res[p:]
+    return res
+
+for i in range(2,15):
+    print(max_res(i),cost(max_res(i)),max_cost(i))
+
+import numpy as np,pylab as pl
+def n_matrix_plot(L):
+    M=max_cost(L)
+    N=list(range(1,L+1))
+    SN=list(permutate(N))
+    for m in range(L-1,M+1):
+        SM=[]
+        for el in SN:
+            if cost(el)==m: 
+                SM.append(el)
+        SM=np.array(SM)
+        print('cost==%d \n'%m,SM,'\n',SM.shape)
+        pl.figure(figsize=(L,SM.shape[0]//2+1))
+        pl.imshow(SM,cmap='jet'); pl.axis('off')
+        pl.tight_layout(); pl.show()
+n_matrix_plot(4)
+
+def cost_plot(L):
+    N=list(range(1,L+1))
+    SN=list(permutate(N))
+    M=[cost(s) for s in SN]
+    pl.figure(figsize=(9,6))
+    pl.plot(M,'o',ms=2)
+    pl.title(str([max_cost(L),max_res(L)]))
+    pl.tight_layout(); pl.show()
+cost_plot(5)
+
+def find_list(L,COST):
+    input_list=list(range(1,L+1))
+    gen=permutate(input_list)
+    curr_list=next(gen)
+    curr_cost=cost(curr_list)
+    while cost(curr_list)!=COST:
+        curr_list=next(gen)
+    return curr_list
+for c in range(4,14):
+    print(c,find_list(5,c),cost(find_list(5,c)))
+
+def permutate(input_list):
+    if len(input_list)<=1:
+        yield input_list
+    else:
+        for p in permutate(input_list[1:]):
+            for i in range(len(input_list)):
+                yield p[:i]+input_list[0:1]+p[i:]
+def cost(N):
+    COST=0
+    for i in range(len(N)-1):
+        j=i+N[i:].index(min(N[i:]))
+        if i==j:
+            COST+=1 
+        else:
+            L=[N[k] for k in range(i)]
+            L+=[N[j-k] for k in range(j-i+1)]
+            L+=[N[k] for k in range(j+1,len(N))]
+            COST+=j-i+1
+            N=L
+    return COST
+def max_cost(L):
+    if L==2: return 2
+    else: return L+max_cost(L-1)
+def max_res(L):    
+    res=[2,1]
+    for i in range(3,L+1):
+        p=len(res)//2
+        res=res[:p]+[i]+res[p:]
+    return res
+def find_list(L,COST):
+    input_list=list(range(1,L+1))
+    gen=permutate(input_list)
+    curr_list=next(gen)
+    curr_cost=cost(curr_list)
+    while cost(curr_list)!=COST:
+        curr_list=next(gen)
+    return curr_list
+T=int(input())
+for t in range(T):
+    S=input().split()
+    L=int(S[0])
+    COST=int(S[1])
+    BASE=list(range(1,L+1))
+    if COST<L-1 or COST>max_cost(L):
+        RES='IMPOSSIBLE'
+    elif COST==max_cost(L):
+        RES=max_res(L)
+    elif COST in range(L-1,2*L-1):
+        RES=BASE[:COST-L+2][::-1]+BASE[COST-L+2:]
+    else:
+        RES=find_list(L,COST)
+    if RES!='IMPOSSIBLE':
+        RES=' '.join(str(el) for el in RES)
+    print('Case #{}: {}'.format(t+1,RES))
+
+"""## TASK 4 MEDIAN SORT
+
+Problem
+
+You want to sort N distinct items, x1,x2,…,xN. Unfortunately, you do not have a way of comparing two of these items. 
+
+You only have a way to, given three of them, find out which one is the median, that is, which one is neither the minimum nor the maximum among the three.
+
+For example, suppose N=5 and you know that:
+
+x1 is the median of {x1,x2,x3}
+
+x2 is the median of {x2,x3,x4}
+
+x3 is the median of {x3,x4,x5}
+
+Then, it is guaranteed that the sorted order of the elements is either x4,x2,x1,x3,x5 or its reverse x5,x3,x1,x2,x4.
+
+Notice that by knowing only medians, it is impossible to distinguish the order of any list from its reverse, 
+
+since the median question has the same result for any three elements in both cases.
+
+Your program will have to find the order of T lists of N elements using at most Q median questions in total (or Q/T queries per list on average). 
+
+In each case, finding either the right order or its reverse is considered correct. The order for each case is generated uniformly at random from all possible orders, and independently of any other information.
+
+Input and output
+
+This is an interactive problem. 
+
+You should make sure you have read the information in the Interactive Problems section of our FAQ.
+
+Initially, the judge will send you a single line containing three integers T, N, and Q: 
+
+the number of test cases, the number of elements to sort within each test case, and the total number of questions you are allowed across all test cases, respectively.
+
+Then, you must process T test cases. 
+
+Each test case consists of a series of question exchanges plus an additional exchange to provide the answer.
+
+For a question exchange, your program must print a single line containing three distinct integers i,j,k all between 1 and N, inclusive, 
+
+which corresponds to asking the judge "which element is the median of the set {xi,xj,xk}?" 
+
+The judge will respond with a single line containing a single integer L, 
+
+meaning that the median of that set is xL (L is always equal to one of i, j, or k). 
+
+If you try to perform a (Q+1)-th question exchange, the judge will simply output -1.
+
+Once you are ready to state the result, print a line containing N integers representing the indices of the elements in sorted or reverse sorted order. 
+
+The judge will respond with a single integer 1 if your answer is correct or -1 if it is not. 
+
+After receiving the judge's answer for the T-th case, your program must finish in time in order to not receive a Time Limit Exceeded error. 
+
+In addition, if you print additional information after receiving the result for the T-th case, you will get a Wrong Answer judgment.
+
+If the judge receives an invalidly formatted line or invalid values from your program at any moment, the judge will print a single number -1. 
+
+After the judge prints -1 for any of the reasons explained above, it will not print any further output. 
+
+If your program continues to wait for the judge after receiving a -1, your program will time out, resulting in a Time Limit Exceeded error. 
+
+Notice that it is your responsibility to have your program exit in time to receive a Wrong Answer judgment instead of a Time Limit Exceeded error. 
+
+As usual, if the memory limit is exceeded, or your program gets a runtime error, you will receive the appropriate judgment.
+
+Limits
+
+Time limit: 40 seconds.
+
+Memory limit: 1 GB.
+
+T=100.
+
+Test Set 1 (Visible Verdict)
+
+N=10.
+
+Q=300⋅T.
+
+Test Set 2 (Visible Verdict)
+
+N=50.
+
+Q=300⋅T.
+
+Test Set 3 (Hidden Verdict)
+
+N=50.
+
+Q=170⋅T.
+
+Testing Tool
+
+You can use this testing tool to test locally or on our platform. 
+To test locally, you will need to run the tool in parallel with your code; you can use our interactive runner for that. 
+For more information, read the instructions in comments in that file, and also check out the Interactive Problems section of the FAQ.
+
+Instructions for the testing tool are included in comments within the tool. 
+
+We encourage you to add your own test cases. Please be advised that although the testing tool is intended to simulate the judging system, 
+
+it is NOT the real judging system and might behave differently. 
+
+If your code passes the testing tool but fails the real judge, please check the Coding section of the FAQ to make sure that you are using the same compiler as us.
+"""
