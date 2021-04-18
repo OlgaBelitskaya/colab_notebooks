a97e9b66547ba6bd8bf049fe8b814f3f46c58fa0
Olga Belitskaya <safuolga@gmail.com>
2021-04-18 11:41:30 +0300

    update image_generator5.ipynb

--- image_generator5.py
+++ image_generator5.py
@@ -7,89 +7,121 @@ Original file is located at
     https://colab.research.google.com/drive/1ZT6ujInkGn_U0cqkPLsoOW8KzTmGQzFi
 """
 
-# Commented out IPython magic to ensure Python compatibility.
-from IPython import display
-def dhtml(st):
-    display.display(display.HTML("""<style>
-    @import url('https://fonts.googleapis.com/css?family=Roboto|Orbitron&effect=3d');      
-    </style><p class='font-effect-3d' onclick='setStyle(this,"#ff6600")'
-    style='font-family:Roboto; font-size:25px; color:#ff355e;'>
-#     %s</p>"""%st+"""<script>
-    function setStyle(element,c) {
-     var docs=document.getElementsByClassName('font-effect-3d');
+#@title Click Headers
+from IPython.display import HTML,display
+def clickhtml(string,emo='✒️',font_color='#ff355e',
+          font_family='times',font_size=22):
+    html_str="""
+    <p class='click_header' 
+    onclick='setStyle(this,
+        &#34;"""+font_color+"""&#34;,
+        &#34;"""+font_family+"""&#34;,
+        &#34;"""+str(font_size)+"""&#34;)'
+    style='text-shadow:3px 3px 3px #ccc; font-family:courier; 
+    font-size:20px; color:#ff33ff;'>
+    """+emo+"""  """+string+"""
+    </p>
+    <script>
+    function setStyle(element,color,family,size) {
+     var docs=document.getElementsByClassName('click_header');
      for (var i=0; i<docs.length; i++) {
-         docs[i].style='font-family:Orbitron; font-size:22px;'; 
-         docs[i].style.color=c;}; };
-    </script>"""))
-dhtml('Code Modules & Parameters')
+         docs[i].style='text-shadow:4px 4px 4px slategray; '+
+                       'font-family:'+family+
+                       '; font-size:'+size+'px; '; 
+         docs[i].style.color=color;}; };
+    </"""+"""script>"""
+    display(HTML(html_str))
 
-!pip install git+https://github.com/tensorflow/docs
+clickhtml('Code Modules & Parameters')
+
+!python3 -m pip install git+https://github.com/tensorflow/docs --q
 
 import numpy as np,pylab as pl,pandas as pd
 import sys,h5py,urllib,zipfile,imageio,PIL
 import tensorflow as tf
-import tensorflow_hub as th
+import tensorflow.keras.layers as tkl
 from tensorflow_docs.vis import embed
+from matplotlib import rcParams
+rcParams['font.family']='serif'
+#'light', 'normal','regular', 'book', 'medium', 'roman', 
+#'semibold', 'demibold', 'demi', 'bold', 'heavy', 'extra bold', 'black'
+rcParams['font.weight']='heavy'
+#'ultra-condensed', 'extra-condensed', 'condensed', 'semi-condensed', 'normal',
+#'semi-expanded', 'expanded', 'extra-expanded', 'ultra-expanded'
+rcParams['font.stretch']='extra-expanded'
+
+pd.DataFrame([list(rcParams.keys()),list(rcParams.values())])\
+.T.iloc[100:130]
 
 seed_size=16; noise_dim=196; epochs=120
 buffer_size=60000; batch_size=256
 norm_img=tf.random.normal([1,noise_dim])
 seed_imgs=tf.random.normal([seed_size,noise_dim])
 
-dhtml('Data')
+clickhtml('Data')
 
 (x,y),(_, _)=tf.keras.datasets.mnist.load_data()
 x=x.reshape(x.shape[0],28,28,1).astype('float32')
+print(x[0].shape,x.mean())
+
+x[0][:28,15:20,:1].reshape(28*5)
+
 x=(x-127.5)/127.5
 digits=tf.data.Dataset.from_tensor_slices(x)\
 .shuffle(buffer_size).batch(batch_size)
 
-dhtml('Deep Convolutional Generative Adversarial Network')
+x[0][:28,15:20,:1].reshape(28*5)
+
+pl.imshow(x[0].reshape(28,28),cmap=pl.cm.bone)
+pl.title('an image example:',color='#ff355e',fontsize=16)
+print(x[0].shape,x.mean())
+
+clickhtml('Deep Convolutional Generative Adversarial Network')
 
 def tfgenerator():
     model=tf.keras.Sequential()
-    model.add(tf.keras.layers\
-    .Dense(7*7*256,use_bias=False,input_shape=(noise_dim,)))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU())
-    model.add(tf.keras.layers.Reshape((7,7,256)))
-    model.add(tf.keras.layers\
-    .Conv2DTranspose(256,(7,7),strides=(1,1),
-                     padding='same',use_bias=False))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU())
-    model.add(tf.keras.layers\
-    .Conv2DTranspose(16,(7,7),strides=(2,2),
-                     padding='same',use_bias=False))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU())
-    model.add(tf.keras.layers\
-    .Conv2DTranspose(1,(7,7),strides=(2,2),
-                     padding='same',use_bias=False,
-                     activation='tanh'))
+    model.add(tkl.Dense(7*7*256,use_bias=False,
+                        input_shape=(noise_dim,)))
+    model.add(tkl.BatchNormalization())
+    model.add(tkl.LeakyReLU())
+    model.add(tkl.Reshape((7,7,256)))
+    model.add(tkl.Conv2DTranspose(
+        256,(7,7),strides=(1,1),
+        padding='same',use_bias=False))
+    model.add(tkl.BatchNormalization())
+    model.add(tkl.LeakyReLU())
+    model.add(tkl.Conv2DTranspose(
+        16,(7,7),strides=(2,2),
+        padding='same',use_bias=False))
+    model.add(tkl.BatchNormalization())
+    model.add(tkl.LeakyReLU())
+    model.add(tkl.Conv2DTranspose(
+        1,(7,7),strides=(2,2),padding='same',
+        use_bias=False,activation='tanh'))
     return model
 tfgenerator=tfgenerator()
 
 def tfdiscriminator():
     model=tf.keras.Sequential()
-    model.add(tf.keras.layers\
-    .Conv2D(16,(7,7),strides=(2,2),
-            padding='same',input_shape=[28,28,1]))
-    model.add(tf.keras.layers.LeakyReLU())
-    model.add(tf.keras.layers.Dropout(.2))
-    model.add(tf.keras.layers\
-    .Conv2D(256,(7,7),strides=(2,2),padding='same'))
-    model.add(tf.keras.layers.LeakyReLU())
-    model.add(tf.keras.layers.Dropout(.2))
-    model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(1))
+    model.add(tkl.Conv2D(
+        16,(7,7),strides=(2,2),
+        padding='same',input_shape=[28,28,1]))
+    model.add(tkl.LeakyReLU())
+    model.add(tkl.Dropout(.2))
+    model.add(tkl.Conv2D(
+        256,(7,7),strides=(2,2),padding='same'))
+    model.add(tkl.LeakyReLU())
+    model.add(tkl.Dropout(.2))
+    model.add(tkl.Flatten())
+    model.add(tkl.Dense(1))
     return model
 tfdiscriminator=tfdiscriminator()
 
 generated_img=tfgenerator(norm_img,training=False)
 pl.imshow(generated_img[0,:,:,0],cmap=pl.cm.bone)
-pl.title(generated_img.shape)
-tfdiscriminator(generated_img)
+pl.title(str(generated_img.shape)+'\n'+\
+         str(tfdiscriminator(generated_img)),
+         color='#ff355e',fontsize=16);
 
 cross_entropy=tf.keras.losses.BinaryCrossentropy(from_logits=True)
 def discriminator_loss(real_output,fake_output):
@@ -101,12 +133,12 @@ def generator_loss(fake_output):
     return cross_entropy(tf.ones_like(fake_output),fake_output)
 generator_optimizer=tf.keras.optimizers.Adam(1e-4)
 discriminator_optimizer=tf.keras.optimizers.Adam(1e-4)
-checkpoint=tf.train.Checkpoint(generator_optimizer=generator_optimizer,
-                               discriminator_optimizer=discriminator_optimizer,
-                               generator=tfgenerator,
-                               discriminator=tfdiscriminator)
+checkpoint=tf.train.Checkpoint(
+    generator_optimizer=generator_optimizer,
+    discriminator_optimizer=discriminator_optimizer,
+    generator=tfgenerator,discriminator=tfdiscriminator)
 
-dhtml('Training')
+clickhtml('Training')
 
 @tf.function
 def train_step(imgs):
@@ -130,44 +162,40 @@ def train_step(imgs):
 
 def generate_images(model,epoch,test_input):
     predictions=model(test_input,training=False)
-    fig=pl.figure(figsize=(4,4))
+    fig=pl.figure(figsize=(6,6))
     for i in range(predictions.shape[0]):
         pl.subplot(4,4,i+1)
-        pl.imshow(predictions[i,:,:,0]*127.5+127.5,
-                  cmap=pl.cm.bone)
+        pl.imshow(predictions[i,:,:,0]*127.5+127.5,cmap=pl.cm.bone)
         pl.axis('off')
     pl.savefig('epoch_{:04d}.png'.format(epoch+1))
-    pl.suptitle('Epoch: %04d'%(epoch+1),
-                color='#ff355e',fontsize=20)
+    pl.suptitle('epoch: %04d'%(epoch+1),color='#ff355e',fontsize=16)
     pl.show()
 
 def train(data,epochs):
     for epoch in range(epochs):
         for image_batch in data:
             train_step(image_batch)
- #           display.clear_output(wait=True)
-        if (epoch+1)%10==0:
+#            display.clear_output(wait=True)
+        if (epoch+1)%20==0:
             generate_images(tfgenerator,epoch,seed_imgs)
-#        if (epoch+1)%10==0:
+#        if (epoch+1)%20==0:
 #            checkpoint.save(file_prefix='')
 #            display.clear_output(wait=True)
 #            generate_images(tfgenerator,epochs,seed_imgs)
 
 train(digits,epochs)
 
-PIL.Image.open('epoch_{:04d}.png'.format(60))
+PIL.Image.open('epoch_{:04d}.png'.format(120))
 
-dhtml('Interpolation')
+clickhtml('Interpolation')
 
 def animate(images):
-    converted_images=np.clip(images*255,0,255)\
-    .astype(np.uint8)
+    converted_images=np.clip(images*255,0,255).astype(np.uint8)
     imageio.mimsave('animation.gif',converted_images)
     return embed.embed_file('animation.gif')
 
 def interpolate_hypersphere(v1,v2,steps):
-    v1norm=tf.norm(v1)
-    v2norm=tf.norm(v2)
+    v1norm=tf.norm(v1); v2norm=tf.norm(v2)
     v2normalized=v2*(v1norm/v2norm)
     vectors=[]
     for step in range(steps):
@@ -179,19 +207,19 @@ def interpolate_hypersphere(v1,v2,steps):
     return tf.stack(vectors)
 
 def interpolate_between_vectors(steps):
-    tf.random.set_seed(1)
+    tf.random.set_seed(12)
     v1=tf.random.normal([noise_dim])
+    tf.random.set_seed(45)
     v2=tf.random.normal([noise_dim])
     vectors=interpolate_hypersphere(v1,v2,steps)
     interpolated_imgs=tfgenerator(vectors,training=False)
-    interpolated_imgs=\
-    tf.image.resize(interpolated_imgs,[128,128])
+    interpolated_imgs=tf.image.resize(interpolated_imgs,[128,128])
     return interpolated_imgs
 
 imgs=interpolate_between_vectors(120)
 animate(imgs)
 
-dhtml('Parameters 2 & Data 2')
+clickhtml('Parameters 2 & Data 2')
 
 seed_size=16; noise_dim=256; img_size=42
 epochs=150; buffer_size=11000; batch_size=128
@@ -209,69 +237,78 @@ zipf.extractall(''); zipf.close()
 f=h5py.File(zf[:-4],'r')
 keys=list(f.keys()); print(keys)
 x=np.array(f[keys[1]],dtype='float32')
-x=tf.image.resize(x,[img_size,img_size]).numpy()
-x=np.dot(x,[.299,.587,.114])
-x=x.reshape(-1,img_size,img_size,1)
-y=np.array(f[keys[2]],dtype='int32')\
-.reshape(-1,1)-1
+print(x[0].shape,x.mean())
+y=np.array(f[keys[2]],dtype='int32').reshape(-1,1)-1
 N=len(y); n=int(.1*N)
 shuffle_ids=np.arange(N)
 np.random.RandomState(23).shuffle(shuffle_ids)
 x,y=x[shuffle_ids],y[shuffle_ids]
+x=tf.image.resize(x,[img_size,img_size]).numpy()
+x=tf.image.rgb_to_grayscale(x).numpy()
+x=np.array(x.reshape(-1,img_size,img_size,1),dtype='float32')
+
+x[1][:42,:10,:1].reshape(42*10)
+
+pl.imshow(255-x[1].reshape(img_size,img_size),cmap=pl.cm.bone)
+pl.title('an image example:',color='#ff355e',fontsize=16)
+print(x[1].shape,x.mean())
 
 x=(x-127.5)/127.5
-pl.imshow((255-x[0]).reshape(img_size,img_size),
-          cmap=pl.cm.bone)
-pl.title(x[0].shape)
 letters=tf.data.Dataset.from_tensor_slices(x)\
 .shuffle(buffer_size).batch(batch_size)
 
-dhtml('DCGAN 2')
+x[0][:42,:20,:1].reshape(42*20)
+
+pl.imshow(1-x[1].reshape(img_size,img_size),cmap=pl.cm.bone)
+pl.title('an image example:',color='#ff355e',fontsize=16)
+print(x[1].shape,x.mean())
+
+clickhtml('DCGAN 2')
 
 def tfgenerator2():
     model=tf.keras.Sequential()
-    model.add(tf.keras.layers\
-    .Dense(7*7*256,use_bias=False,input_shape=(noise_dim,)))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU())
-    model.add(tf.keras.layers.Reshape((7,7,256)))
-    model.add(tf.keras.layers\
-    .Conv2DTranspose(256,(7,7),strides=(3,3),
-                     padding='same',use_bias=False))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU())
-    model.add(tf.keras.layers\
-    .Conv2DTranspose(32,(7,7),strides=(2,2),
-                     padding='same',use_bias=False))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU())
-    model.add(tf.keras.layers\
-    .Conv2DTranspose(1,(7,7),strides=(1,1),
-                     padding='same',use_bias=False,
-                     activation='tanh'))
+    model.add(tkl.Dense(
+        7*7*256,use_bias=False,input_shape=(noise_dim,)))
+    model.add(tkl.BatchNormalization())
+    model.add(tkl.LeakyReLU())
+    model.add(tkl.Reshape((7,7,256)))
+    model.add(tkl.Conv2DTranspose(
+        256,(7,7),strides=(3,3),
+        padding='same',use_bias=False))
+    model.add(tkl.BatchNormalization())
+    model.add(tkl.LeakyReLU())
+    model.add(tkl.Conv2DTranspose(
+        32,(7,7),strides=(2,2),
+        padding='same',use_bias=False))
+    model.add(tkl.BatchNormalization())
+    model.add(tkl.LeakyReLU())
+    model.add(tkl.Conv2DTranspose(
+        1,(7,7),strides=(1,1),padding='same',
+        use_bias=False,activation='tanh'))
     return model
 tfgenerator2=tfgenerator2()
 
 def tfdiscriminator2():
     model=tf.keras.Sequential()
-    model.add(tf.keras.layers\
-    .Conv2D(32,(7,7),strides=(2,2),padding='same',
-            input_shape=[img_size,img_size,1]))
-    model.add(tf.keras.layers.LeakyReLU())
-    model.add(tf.keras.layers.Dropout(.2))
-    model.add(tf.keras.layers\
-    .Conv2D(256,(7,7),strides=(2,2),padding='same'))
-    model.add(tf.keras.layers.LeakyReLU())
-    model.add(tf.keras.layers.Dropout(.2))
-    model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(1))
+    model.add(tkl.Conv2D(
+        32,(7,7),strides=(2,2),padding='same',
+        input_shape=[img_size,img_size,1]))
+    model.add(tkl.LeakyReLU())
+    model.add(tkl.Dropout(.2))
+    model.add(tkl.Conv2D(
+        256,(7,7),strides=(2,2),padding='same'))
+    model.add(tkl.LeakyReLU())
+    model.add(tkl.Dropout(.2))
+    model.add(tkl.Flatten())
+    model.add(tkl.Dense(1))
     return model
 tfdiscriminator2=tfdiscriminator2()
 
 generated_img=tfgenerator2(norm_img,training=False)
 pl.imshow(generated_img[0,:,:,0],cmap=pl.cm.bone)
-pl.title(generated_img.shape);
-tfdiscriminator2(generated_img)
+pl.title(str(generated_img.shape)+'\n'+\
+         str(tfdiscriminator2(generated_img)),
+         color='#ff355e',fontsize=16);
 
 cross_entropy=tf.keras.losses.BinaryCrossentropy(from_logits=True)
 def discriminator_loss(real_output,fake_output):
@@ -284,7 +321,7 @@ def generator_loss(fake_output):
 generator_optimizer2=tf.keras.optimizers.Adam(1e-3)
 discriminator_optimizer2=tf.keras.optimizers.Adam(1e-3)
 
-dhtml('Training')
+clickhtml('Training')
 
 @tf.function
 def train_step2(imgs):
@@ -308,15 +345,13 @@ def train_step2(imgs):
 
 def generate_images2(model,epoch,test_input):
     predictions=model(test_input,training=False)
-    fig=pl.figure(figsize=(4,4))
+    fig=pl.figure(figsize=(6,6))
     for i in range(predictions.shape[0]):
         pl.subplot(4,4,i+1)
-        pl.imshow(127.5-predictions[i,:,:,0]*127.5,
-                  cmap=pl.cm.bone)
+        pl.imshow(127.5-predictions[i,:,:,0]*127.5,cmap=pl.cm.bone)
         pl.axis('off')
     pl.savefig('epoch_{:04d}.png'.format(epoch+1))
-    pl.suptitle('Epoch: %04d'%(epoch+1),
-                color='#ff355e',fontsize=20)
+    pl.suptitle('epoch: %04d'%(epoch+1),color='#ff355e',fontsize=16)
     pl.show()
 
 def train2(data,epochs):
@@ -328,11 +363,12 @@ def train2(data,epochs):
 
 train2(letters,epochs)
 
-PIL.Image.open('epoch_{:04d}.png'.format(130))
+PIL.Image.open('epoch_{:04d}.png'.format(150))
 
 def interpolate_between_vectors2(steps):
-    tf.random.set_seed(123)
+    tf.random.set_seed(1)
     v1=tf.random.normal([noise_dim])
+    tf.random.set_seed(123)
     v2=tf.random.normal([noise_dim])
     vectors=interpolate_hypersphere(v1,v2,steps)
     interpolated_imgs=tfgenerator2(vectors,training=False)
